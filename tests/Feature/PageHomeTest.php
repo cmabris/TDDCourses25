11a7e138a3f8a41e2792ea0110b8620f58152a87
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-11-11 16:18:10 +0100

    Paso 5

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -2,15 +2,28 @@
 
 use App\Models\Course;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
 it('show courses overview', function () {
     // Arrange
-    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A']);
-    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B']);
-    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C']);
+    Course::factory()->create([
+        'title' => 'Course A',
+        'description' => 'Description Course A',
+        'released_at' => Carbon::now()
+    ]);
+    Course::factory()->create([
+        'title' => 'Course B',
+        'description' => 'Description Course B',
+        'released_at' => Carbon::now()
+    ]);
+    Course::factory()->create([
+        'title' => 'Course C',
+        'description' => 'Description Course C',
+        'released_at' => Carbon::now()
+    ]);
 
     // Act & Assert
     get(route('home'))
@@ -26,17 +39,29 @@ it('show courses overview', function () {
 
 it('shows only released courses', function () {
     // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B']);
 
-    // Act
-
-    // Assert
+    // Act & Assert
+    get(route('home'))
+        ->assertSeeText([
+            'Course A',
+        ])
+        ->assertDontSee([
+            'Course B',
+        ]);
 
 });
 
 it('shows courses by release date', function () {
     // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B', 'released_at' => Carbon::now()]);
 
-    // Act
-
-    // Assert
+    // Act & Assert
+    get(route('home'))
+        ->assertSeeInOrder([
+            'Course B',
+            'Course A',
+        ]);
 });
