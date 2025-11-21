ee455f9359d240fd52bb58a5c38428b7165c1345
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-11-21 20:04:20 +0100

    Paso 14 con ficheros

--- app/Http/Controllers/PageVideosController.php
+++ app/Http/Controllers/PageVideosController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-class PageVideosController extends Controller{
+class PageVideosController extends Controller
+{
     public function __invoke()
     {
-        //
+
     }
 }
--- tests/Feature/Pages/PageDashboardTest.php
+++ tests/Feature/Pages/PageDashboardTest.php
@@ -4,6 +4,7 @@ use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
@@ -38,24 +39,45 @@ it('lists purchased courses', function () {
 
 it('does not list other courses', function () {
     // Arrange
+    $user = User::factory()->create();
+    $course = Course::factory()->create();
 
-    // Act
-
-    // Assert
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertDontSeeText($course->title);
 });
 
 it('shows latest purchased course first', function () {
     // Arrange
+    $user = User::factory()->create();
+    $firstPurchasedCourse = Course::factory()->create();
+    $lastPurchasedCourse = Course::factory()->create();
 
-    // Act
+    $user->courses()->attach($firstPurchasedCourse, ['created_at' => Carbon::yesterday()]);
+    $user->courses()->attach($lastPurchasedCourse, ['created_at' => Carbon::now()]);
 
-    // Assert
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertSeeInOrder([
+            $lastPurchasedCourse->title,
+            $firstPurchasedCourse->title,
+        ]);
 });
 
 it('includes link to product videos', function () {
     // Arrange
+    $user = User::factory()
+        ->has(Course::factory())
+        ->create();
 
-    // Act
-
-    // Assert
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertSeeText('Watch videos')
+        ->assertSee(route('page.course-videos', Course::first()));
 });
